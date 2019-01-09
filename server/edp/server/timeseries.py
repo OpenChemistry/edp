deac84b304cc2dd47895c82fd65650a6aec56c7e
Alessandro Genova <alessandro.genova@kitware.com>
2019-01-09 15:13:48 -0500

    Add endpoint to get the fitted timeseries

--- server/edp/server/timeseries.py
+++ server/edp/server/timeseries.py
@@ -10,8 +10,7 @@ from girder.plugins.edp.models.sample import Sample as SampleModel
 from girder.plugins.edp.models.project import Project as ProjectModel
 from girder.plugins.edp.models.composite import Composite as CompositeModel
 from girder.plugins.edp.models.timeseries import TimeSeries as TimeSeriesModel
-
-
+from girder.plugins.edp.utils.spline import make_models
 
 class TimeSeries(resource.create(TimeSeriesModel)):
 
@@ -28,9 +27,11 @@ class TimeSeries(resource.create(TimeSeriesModel)):
             model=SampleModel, destName='sample',
             level=AccessType.READ, paramType='path')
         .param('runId', '', required=True)
+        .param('fitted', 'If true return a fit model of the data instead of the actual points',
+            paramType='query', dataType='boolean', default=False)
         .pagingParams(defaultSort='sampleNum')
     )
-    def find(self, project, composite, sample, runId, offset=0, limit=None, sort=None):
+    def find(self, project, composite, sample, runId, fitted=False, offset=0, limit=None, sort=None):
         query = {
             'sampleId': sample['_id'],
             'runId': {
@@ -41,7 +42,12 @@ class TimeSeries(resource.create(TimeSeriesModel)):
         cursor = TimeSeriesModel().find(query=query, offset=offset,
                                         sort=sort, user=getCurrentUser())
 
-        return  list(TimeSeriesModel().filterResultsByPermission(cursor=cursor, user=getCurrentUser(),
+        timeseries = list(TimeSeriesModel().filterResultsByPermission(cursor=cursor, user=getCurrentUser(),
                                                                  level=AccessType.READ,
                                                                  limit=limit, offset=offset))
 
+        if fitted:
+            for t in timeseries:
+                t['data'] = make_models(t['data'], (1, 3), 0.15)
+
+        return timeseries
---
+++ server/edp/server/utils/spline.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy.interpolate import UnivariateSpline as Spline
+
+def make_models(data, ks, s):
+    model = {}
+    for key in data.keys():
+        for k in ks:
+            spl = smooth_spline(data, key, k, s)
+            keep_prev = False
+            prev_spl = model.get(key, None)
+            if prev_spl is not None:
+                # Check number of nodes:
+                m = len(prev_spl.get_knots())
+                n = len(spl.get_knots())
+                if m < n:
+                    keep_prev = True
+                elif m == n:
+                    r = prev_spl.get_residual()
+                    s = spl.get_residual()
+                    if r < s:
+                        keep_prev = True
+
+            if not keep_prev:
+                model[key] = spl
+
+    for key in model.keys():
+        params = model[key]._eval_args
+        model[key] = {
+            'knots': list(params[0]),
+            'coeffs': list(params[1]),
+            'k': params[2]
+        }
+
+    return model
+
+def smooth_spline(data, key, k, smooth):
+    span = max(data[key]) - min(data[key])
+    y = np.array(data[key])
+    x = np.linspace(0, 1, num=len(y), endpoint=False)
+    spl = Spline(x, y, k=k, s=smooth * span * k)
+    return spl
