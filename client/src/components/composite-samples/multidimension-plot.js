ea89954fa06011b31e73b626f17c5c8cd2919681
Alessandro Genova <alessandro.genova@kitware.com>
2019-10-23 10:52:40 -0400

    Fix multidimensional plot label position

--- client/src/components/composite-samples/multidimension-plot.js
+++ client/src/components/composite-samples/multidimension-plot.js
@@ -145,7 +145,7 @@ class MultidimensionPlotComponent extends Component {
   render() {
     return (
       <div
-        style={{width: '100%', height: '22.5rem'}}
+        style={{width: '100%', height: '22.5rem', position: 'relative', overflow: 'hidden'}}
         ref={(ref) => {this.plotElement = ref;}}
       >
       </div>
