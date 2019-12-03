84ca41682165bf9ac108b6e89d765963e1b34223
Chris Harris <chris.harris@kitware.com>
2019-12-03 13:14:15 -0500

    Refactor and add missing field

--- server/edp/server/sample.py
+++ server/edp/server/sample.py
@@ -13,6 +13,72 @@ from girder.plugins.edp.models.composite import Composite as CompositeModel
 from girder.plugins.edp.models.platemap import PlateMap as PlateMapModel
 
 
+def find_samples(project, composite, runId=None, platemapId=None, elements=None, offset=0, limit=None, sort=None):
+    platemap = PlateMapModel().load(ObjectId(platemapId), level=AccessType.READ, user=getCurrentUser())
+
+    and_exp = [{
+            '$eq': ['$plateId', platemap['plateId']]
+    }]
+    match_samples = {
+        '$match': {
+            '$expr': {
+                '$and': and_exp
+            },
+        }
+    }
+
+    if elements is not None:
+        and_exp.append({
+            '$setIsSubset': [
+                '$composition.elements', elements
+            ]
+        })
+
+    lookup_fom_match = {
+        '$expr': {
+            '$and': [{
+                '$eq': ["$sampleId",  "$$id"]
+                }]
+        }
+    }
+
+    if runId is not None:
+        lookup_fom_match['$expr']['$and'].append({
+            '$eq': ['$runId', ObjectId(runId)]
+        })
+
+    lookup_fom =   {
+        '$lookup':{
+            'from': 'edp.fom',
+            'let': {
+                'id': '$_id'
+            },
+            'pipeline': [{
+                '$match': lookup_fom_match
+            },
+            {
+                '$project': {
+                    'sampleId': 0
+                }
+            }
+            ],
+            'as': 'fom'
+            }
+    }
+
+    exclude_empty =  {
+        "$match": {
+            "fom": {"$ne": [] }
+        }
+    }
+
+    pipeline = [match_samples, lookup_fom, exclude_empty]
+    cursor = SampleModel().collection.aggregate(pipeline)
+
+    return  list(SampleModel().filterResultsByPermission(cursor=cursor, user=getCurrentUser(),
+                                                            level=AccessType.READ,
+                                                            limit=limit, offset=offset))
+
 
 class Sample(resource.create(SampleModel)):
 
@@ -31,67 +97,4 @@ class Sample(resource.create(SampleModel)):
         .pagingParams(defaultSort='sampleNum')
     )
     def find(self, project, composite, runId, platemapId, elements, offset=0, limit=None, sort=None):
-        platemap = PlateMapModel().load(ObjectId(platemapId), level=AccessType.READ, user=getCurrentUser())
-
-        and_exp = [{
-                '$eq': ['$plateId', platemap['plateId']]
-        }]
-        match_samples = {
-            '$match': {
-                '$expr': {
-                    '$and': and_exp
-                },
-            }
-        }
-
-        if elements is not None:
-            and_exp.append({
-                '$setIsSubset': [
-                    '$composition.elements', elements
-                ]
-            })
-
-        lookup_fom_match = {
-            '$expr': {
-                '$and': [{
-                    '$eq': ["$sampleId",  "$$id"]
-                    }]
-            }
-        }
-
-        if runId is not None:
-            lookup_fom_match['$expr']['$and'].append({
-                '$eq': ['$runId', ObjectId(runId)]
-            })
-
-        lookup_fom =   {
-            '$lookup':{
-               'from': 'edp.fom',
-               'let': {
-                   'id': '$_id'
-                },
-               'pipeline': [{
-                   '$match': lookup_fom_match
-                },
-                {
-                    '$project': {
-                        'sampleId': 0
-                    }
-                }
-               ],
-               'as': 'fom'
-             }
-        }
-
-        exclude_empty =  {
-            "$match": {
-                "fom": {"$ne": [] }
-            }
-        }
-
-        pipeline = [match_samples, lookup_fom, exclude_empty]
-        cursor = SampleModel().collection.aggregate(pipeline)
-
-        return  list(SampleModel().filterResultsByPermission(cursor=cursor, user=getCurrentUser(),
-                                                             level=AccessType.READ,
-                                                             limit=limit, offset=offset))
+        return find_samples(project, composite, runId, platemapId, elements, offset, limit, sort)
