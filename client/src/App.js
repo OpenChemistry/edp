a6c0216b4e51cf00f7d3046bde8d85109fcc58fe
Chris Harris <chris.harris@kitware.com>
2020-03-05 11:31:38 -0500

    Enable dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { ConnectedRouter } from 'connected-react-router'
 
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import { history } from './redux/store';
 import PrivateRoute from './containers/privateRoute';
@@ -51,6 +53,12 @@ const appStyles = theme => ({
   }
 });
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+});
+
 class App extends Component {
   render() {
     let development = false;
@@ -59,48 +67,50 @@ class App extends Component {
     }
     const {classes} = this.props;
     return (
-      <div className={classes.root}>
-        <Head />
-        <CssBaseline />
-        <Header />
-        <ConnectedRouter history={history}>
-          <div className={classes.content}>
-            <BreadCrumb/>
-            <Switch>
-              <PrivateRoute path={'/:url0/:action(add)'} exact component={ItemEdit} />
-              <PrivateRoute path={'/:url0/:id0/:action(edit)'} exact component={ItemEdit} />
-              <PublicRoute path={'/:url0/:id0'} exact component={ItemView} />
+      <ThemeProvider theme={darkTheme}>
+        <div className={classes.root}>
+          <Head />
+          <CssBaseline />
+          <Header />
+          <ConnectedRouter history={history}>
+            <div className={classes.content}>
+              <BreadCrumb/>
+              <Switch>
+                <PrivateRoute path={'/:url0/:action(add)'} exact component={ItemEdit} />
+                <PrivateRoute path={'/:url0/:id0/:action(edit)'} exact component={ItemEdit} />
+                <PublicRoute path={'/:url0/:id0'} exact component={ItemView} />
 
-              <PrivateRoute path={'/:url0/:id0/:url1/:action(add)'} exact component={ItemEdit} />
-              <PrivateRoute path={'/:url0/:id0/:url1/:id1/:action(edit)'} exact component={ItemEdit} />
-              <PublicRoute path={'/:url0/:id0/:url1/:id1/:action(samples)'} exact component={CompositeSamplesView} />
-              <PublicRoute path={'/:url0/:id0/:url1/:id1/:action(learning)'} exact component={CompositeSamplesLearning} />
-              <PublicRoute path={'/:url0/:id0/:url1/:id1'} exact component={ItemView} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:action(add)'} exact component={ItemEdit} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:id1/:action(edit)'} exact component={ItemEdit} />
+                <PublicRoute path={'/:url0/:id0/:url1/:id1/:action(samples)'} exact component={CompositeSamplesView} />
+                <PublicRoute path={'/:url0/:id0/:url1/:id1/:action(learning)'} exact component={CompositeSamplesLearning} />
+                <PublicRoute path={'/:url0/:id0/:url1/:id1'} exact component={ItemView} />
 
-              <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:action(add)'} exact component={ItemEdit} />
-              <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:action(edit)'} exact component={ItemEdit} />
-              <PublicRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2'} exact component={ItemView} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:action(add)'} exact component={ItemEdit} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:action(edit)'} exact component={ItemEdit} />
+                <PublicRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2'} exact component={ItemView} />
 
-              <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:action(add)'} exact component={ItemEdit} />
-              <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:id3/:action(edit)'} exact component={ItemEdit} />
-              <PublicRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:id3'} exact component={ItemView} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:action(add)'} exact component={ItemEdit} />
+                <PrivateRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:id3/:action(edit)'} exact component={ItemEdit} />
+                <PublicRoute path={'/:url0/:id0/:url1/:id1/:url2/:id2/:url3/:id3'} exact component={ItemView} />
 
-              <PublicRoute path={'/search'} exact component={SearchContainer} />
+                <PublicRoute path={'/search'} exact component={SearchContainer} />
 
-              <PublicRoute path={'/multidimension'} exact component={MultidimensionContainer} />
-              <PublicRoute path={'/multidimension-learning'} exact component={MultidimensionLearningContainer} />
+                <PublicRoute path={'/multidimension'} exact component={MultidimensionContainer} />
+                <PublicRoute path={'/multidimension-learning'} exact component={MultidimensionLearningContainer} />
 
-              <PublicRoute path={ROOT_ROUTE} exact component={ItemView} />
-            </Switch>
+                <PublicRoute path={ROOT_ROUTE} exact component={ItemView} />
+              </Switch>
+            </div>
+          </ConnectedRouter>
+          <div className={classes.footer}>
+            <Footer />
           </div>
-        </ConnectedRouter>
-        <div className={classes.footer}>
-          <Footer />
+          <authUI.LoginOptions girder={development}/>
+          <authUI.GirderLogin/>
+          <authUI.OauthRedirect/>
         </div>
-        <authUI.LoginOptions girder={development}/>
-        <authUI.GirderLogin/>
-        <authUI.OauthRedirect/>
-      </div>
+      </ThemeProvider>
     );
   }
 }
